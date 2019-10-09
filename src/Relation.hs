a120876b3745f442c71a89a968dd4322e4f0a59e
Nuxii <nuxii09@gmail.com>
2019-10-09 02:26:28 +0100

    Added reflexive closure.

--- src/Relation.hs
+++ src/Relation.hs
@@ -1,8 +1,14 @@
-module Relation (Relation, symmetric) where
+module Relation (Relation, symmetric, reflexive) where
 
 -- |A type alias which describes a relation information.
 type Relation a = [(a, a)]
 
 -- |Computes the symmetric closure for this graph.
 symmetric :: Eq a => Relation a -> Relation a
-symmetric r = r ++ [(y, x) | (x, y) <- r, not ((y, x) `elem` r)]
+symmetric r = r ++ [(y, x) | (x, y) <- r, (y, x) `notElem` r]
+
+-- |Computes the reflexive closure for this graph.
+reflexive :: Eq a => Relation a -> Relation a
+reflexive r = r
+        ++ [(x, x) | (x, _) <- r, (x, x) `notElem` r]
+        ++ [(y, y) | (_, y) <- r, (y, y) `notElem` r]
