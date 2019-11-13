951140344a03eb7b360cc2fd4f15c421101141e9
Nuxii <nuxii09@gmail.com>
2019-11-13 18:04:24 +0000

    Simplified Graph module.

--- src/Graph.hs
+++ src/Graph.hs
@@ -1,10 +1,10 @@
 -- |Suppies functions for generating and manipulating graph structures. Assumes all graphs have no duplicate elements.
 module Graph (Graph, Edge,
-        {-add, remove, neighbours,
+        add, remove, neighbours,
         symmetric, reflexive, transitive,
-        compose, transpose,-}
+        compose, transpose,
         domain, permutation, isSup, isSub,
-        union, dunion, intersection, difference)
+        dunion, union, intersection, difference)
     where
     import Data.List (nub, concat)
     
@@ -14,29 +14,65 @@ module Graph (Graph, Edge,
     -- |A type alias for transitions between two nodes.
     type Edge a = (a, a)
 
+    -- |Adds a new edge to the graph.
+    add :: (Eq a) => Graph a -> Edge a -> Graph a
+    add r e = union r [e]
+    
+    -- |Removes a node from the graph. Any connected edges are also removed.
+    remove :: (Eq a) => Graph a -> a -> Graph a
+    remove r v = [(u, w) | (u, w) <- r, u /= v && w /= v]
+
+    -- |Computes the list of neighbours of this node.
+    neighbours :: (Eq a) => Graph a -> a -> [a]
+    neighbours r v = [w | (u, w) <- r, u == v]
+
+    -- |Computes the symmetric closure of this graph.
+    symmetric :: (Eq a) => Graph a -> Graph a
+    symmetric r = union r (transpose r)
+
+    -- |Computes the reflexive closure of this graph.
+    reflexive :: (Eq a) => Graph a -> Graph a
+    reflexive r = union r [(x, x) | x <- domain r]
+
+    -- |Computes the transitive closure of this graph.
+    transitive :: (Eq a) => Graph a -> Graph a
+    transitive r = if permutation r' r
+        then r
+        else transitive r'
+        where 
+        r' = union r (compose r r)
+
+    -- |Computes the composition of two graphs.
+    compose :: (Eq a) => Graph a -> Graph a -> Graph a
+    compose r s = [(v, w) | (v, u) <- r, (u', w) <- s, u == u']
+
+    -- |Computes the transpose of this graph.
+    transpose :: Graph a -> Graph a
+    transpose r = [(u, v) | (v, u) <- r]
+
     -- |Computes the domain of this graph.
     domain :: (Eq a) => Graph a -> [a]
-    domain r = (nub . concat) [[a, b] | (a, b) <- r]
+    domain r = (nub . concat) [[v, u] | (v, u) <- r]
 
     -- |Returns whether a graph is a permutation of another.
     permutation :: (Eq a) => Graph a -> Graph a -> Bool
-    permutation r s = r `isSub` s && r `isSup` s
+    permutation r s = isSub r s && isSup r s
 
     -- |Returns whether a graph is a superset of another.
     isSup :: (Eq a) => Graph a -> Graph a -> Bool
-    isSup r s = s `isSub` r
+    isSup r s = isSub s r
 
     -- |Returns whether a graph is a subset of another.
     isSub :: (Eq a) => Graph a -> Graph a -> Bool
     isSub r s = all (`elem` s) r
 
-    -- |Computes the union (OR) of two graphs.
-    union :: (Eq a) => Graph a -> Graph a -> Graph a
-    union r s = dunion r s ++ intersection r s
-
     -- |Computes the symmetric difference (XOR) of two graphs.
     dunion :: (Eq a) => Graph a -> Graph a -> Graph a
-    dunion r s = difference r s ++ difference s r
+    dunion r s = difference (union r s) (intersection r s)
+
+    -- |Computes the union (OR) of two graphs.
+    union :: (Eq a) => Graph a -> Graph a -> Graph a
+    union r s = difference r s ++ s
 
     -- |Computes the intersection (AND) of two graphs.
     intersection :: (Eq a) => Graph a -> Graph a -> Graph a
@@ -45,42 +81,3 @@ module Graph (Graph, Edge,
     -- |Computes the set difference of two graphs.
     difference :: (Eq a) => Graph a -> Graph a -> Graph a
     difference r s = [x | x <- r, x `notElem` s]
-
-    ---old---
-
-    {-
-    -- |Adds a new edge to the graph.
-    add :: (Node a) => Graph a -> (a, a) -> Graph a
-    add r n = r `union` [n]
-    
-    -- |Removes a node from the graph. Any connected edges are also removed.
-    remove :: (Node a) => Graph a -> a -> Graph a
-    remove r x = filter (\(a, b) -> a /= x && b /= x) r
-
-    -- |Computes the list of neighbours of this node.
-    neighbours :: (Node a) => Graph a -> a -> [a]
-    neighbours r x = [b | (a, b) <- r, a == x]
-
-    -- |Computes the symmetric closure of this graph.
-    symmetric :: (Node a) => Graph a -> Graph a
-    symmetric r = r `union` transpose r
-
-    -- |Computes the reflexive closure of this graph.
-    reflexive :: (Node a) => Graph a -> Graph a
-    reflexive r = r `union` [(x, x) | x <- domain r]
-
-    -- |Computes the transitive closure of this graph.
-    transitive :: (Node a) => Graph a -> Graph a
-    transitive r = if r `permutation` r'
-        then r
-        else transitive r'
-        where 
-        r' = r `union` (r `compose` r)
-
-    -- |Computes the composition of two graphs.
-    compose :: (Node a) => Graph a -> Graph a -> Graph a
-    compose r s = [(a, c) | (a, b) <- r, (b', c) <- s, b == b']
-
-    -- |Computes the transpose of this graph.
-    transpose :: (Node a) => Graph a -> Graph a
-    transpose r = [(y, x) | (x, y) <- r]-}
