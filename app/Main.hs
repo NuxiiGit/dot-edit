7969ed5a898dc4658b819f31b4e875dd9100fff1
Nuxii <nuxii09@gmail.com>
2020-01-06 16:13:38 +0000

    Implemented basic main function which reads and writes.

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,17 @@
 import Graph
 import Dot
 
+import System.IO
+import System.Directory
+import Control.Monad
+
 main :: IO ()
 main = do
-    c <- getLine
-    putStrLn $ c
+    context <- readFile "resources/test.txt"
+    let dir = "bin/graphs/"
+    createDirectoryIfMissing True dir
+    writeFile (dir ++ "graph.dot") context
+
 
 numbers :: Graph Int
 numbers = [(3, 1), (0, 2), (2, 4), (3, 4), (3, 0)]
