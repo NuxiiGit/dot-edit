a178cde5cfcf4e9d15ad5e8886c0b9ded482aa68
NuxiiGit <nuxii09@gmail.com>
2020-07-23 22:08:58 +0100

    added breadth first function

--- lib_core/Pathing.hs
+++ lib_core/Pathing.hs
@@ -8,6 +8,9 @@ module Pathing (module Pathing)
     depthFirst :: (Ord a) => Graph a -> a -> Graph a
     depthFirst = traversal (\old new -> sort new ++ old)
 
+    -- |Computes the breadth-first traversal of a graph.
+    breadthFirst = traversal (\old new -> old ++ sort new)
+
     -- |Computes the best-first traversal of a graph.
     bestFirst :: (Ord a) => Graph a -> a -> Graph a
     bestFirst = traversal (\old new -> prioritise $ old ++ new)
@@ -24,39 +27,3 @@ module Pathing (module Pathing)
             else search frontier (e : visits)
             where
             frontier = es `f` branches r (snd e)
-
-{-
-    -- |Predicate for computing the depth-first traversal of a tree.
-    depthf :: (Ord a) => [Tree a] -> [Tree a]
-    depthf = sortBy order
-        where
-        order (Node d v _) (Node d' v' _)
-            | d < d' = GT
-            | d > d' = LT
-            | v < v' = LT
-            | v > v' = GT
-            | otherwise = EQ
-
-    -- |Predicate for computing the breadth-first traversal of a tree.
-    breadthf :: (Ord a) => [Tree a] -> [Tree a]
-    breadthf = sortBy order
-        where
-        order (Node d v _) (Node d' v' _)
-            | d < d' = LT
-            | d > d' = GT
-            | v < v' = LT
-            | v > v' = GT
-            | otherwise = EQ
-
-    -- |Computes the best-first traversal using `f` to sort the frontier each step.
-    traversal :: (Eq a) => ([Tree a] -> [Tree a]) -> Tree a -> [a]
-    traversal f t = reverse $ search [t] []
-        where
-        search [] visits = visits
-        search (Node _ v neighbours : ts) visits = if visited v
-            then search ts visits
-            else search frontier $ v : visits
-            where
-            frontier = f $ filter (\(Node _ v _) -> not . visited $ v) neighbours ++ ts
-            visited = (`elem` visits)
--}
