c2834a600dbcfa1005359b708e5646db0c379bf3
NuxiiGit <nuxii09@gmail.com>
2020-07-23 22:03:54 +0100

    refactored best first

--- lib_core/Pathing.hs
+++ lib_core/Pathing.hs
@@ -6,15 +6,26 @@ module Pathing (module Pathing)
 
     -- |Computes the best-first traversal of a graph.
     bestFirst :: (Ord a) => Graph a -> a -> Graph a
-    bestFirst r root = reverse $ search (prioritise $ branches r root) []
+    bestFirst r root = search (prioritise $ branches r root) []
         where
         prioritise = sortBy $ \x y -> compare (snd x) (snd y)
-        search [] visits = visits
+        search [] visits = reverse visits
         search (e : es) visits = if any (\x -> snd x == snd e) visits
             then search es visits
             else search frontier (e : visits)
             where
             frontier = prioritise $ es ++ branches r (snd e)
+
+    -- |Computes the traversal of a graph, where `f` details how to concatenate the current and new frontiers.
+    traversal :: (Eq a) => (Graph a -> Graph a -> Graph a) -> Graph a -> a -> Graph a
+    traversal f r root = search ([] `f` branches r root) []
+        where
+        search [] visits = reverse visits
+        search (e : es) visits = if any (\x -> snd x == snd e) visits
+            then search es visits
+            else search frontier (e : visits)
+            where
+            frontier = es `f` branches r (snd e)
 
 {-
     -- |Predicate for computing the depth-first traversal of a tree.
